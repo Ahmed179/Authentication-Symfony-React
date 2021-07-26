2e373cd05a93fd045c5b420effe81aeca787a0ad
Ahmed <ragnarokyt179@gmail.com>
2021-07-26 16:58:46 +0300

    ui update

--- social-media-frontend/src/components/Navbar/index.js
+++ social-media-frontend/src/components/Navbar/index.js
@@ -1,51 +1,70 @@
-import { NavLink } from "react-router-dom"
-import HomeIcon from "@material-ui/icons/Home"
-import { useHistory } from "react-router-dom"
-import "./style.css"
-// import FeaturedPlayListOutlinedIcon from "@material-ui/icons/FeaturedPlayListOutlined"
-// import AssignmentTurnedInOutlinedIcon from "@material-ui/icons/AssignmentTurnedInOutlined"
-// import PeopleAltOutlinedIcon from "@material-ui/icons/PeopleAltOutlined"
-// import NotificationsOutlinedIcon from "@material-ui/icons/NotificationsOutlined"
-// import SearchIcon from "@material-ui/icons/Search"
-// import Avatar from "@material-ui/core/Avatar"
-// import LanguageIcon from "@material-ui/icons/Language"
+import React, { useState } from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import FeaturedPlayListOutlinedIcon from "@material-ui/icons/FeaturedPlayListOutlined";
+import AssignmentTurnedInOutlinedIcon from "@material-ui/icons/AssignmentTurnedInOutlined";
+import PeopleAltOutlinedIcon from "@material-ui/icons/PeopleAltOutlined";
+import NotificationsOutlinedIcon from "@material-ui/icons/NotificationsOutlined";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import {useHistory} from 'react-router-dom'
+
+import "./style.css";
+import { Avatar, Button, Input } from "@material-ui/core";
+import { useSelector } from "react-redux";
+
+import { ExpandMore, Link } from "@material-ui/icons";
+
+
+
 
 function Navbar() {
-  const history = useHistory()
+    const history = useHistory()
 
-  const logout = () => {
+   const logout = () => {
     localStorage.removeItem("token")
     history.push("/login")
   }
 
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
+    <div className="qHeader">
+      <div className="qHeader__logo">
+       <img src="https://i.ibb.co/RDvh4jM/ahmed.png" alt="ahmed" border="0"/>
+      </div>
+      
+      <div className="qHeader__icons">
+        <div className="active qHeader__icon">
           <HomeIcon />
-        </NavLink>
-        {/* <NavLink className="navbar-brand" to="/">
-          <FeaturedPlayListOutlinedIcon />
-        </NavLink>
-        <NavLink className="navbar-brand" to="/">
-          <AssignmentTurnedInOutlinedIcon />
-        </NavLink>
-        <NavLink className="navbar-brand" to="/">
-          <PeopleAltOutlinedIcon />
-        </NavLink>
-        <NavLink className="navbar-brand" to="/">
-          <NotificationsOutlinedIcon />
-        </NavLink>
-        <div className="search_input">
-          <SearchIcon />
-          <input type="text" placeholder="Search Quora" />
         </div>
-        <div className="avatar_items">
-          <div className="avatar_item">
-            <Avatar />
-          </div>
-          <LanguageIcon />
-        </div> */}
+        <div className="qHeader__icon">
+          <FeaturedPlayListOutlinedIcon />
+        </div>
+        <div className="qHeader__icon">
+          <AssignmentTurnedInOutlinedIcon />
+        </div>
+        <div className="qHeader__icon">
+          <PeopleAltOutlinedIcon />
+        </div>
+        <div className="qHeader__icon">
+          <NotificationsOutlinedIcon />
+        </div>
+      </div>
+      
+      <div className="qHeader__input">
+        <SearchIcon />
+        <input type="text" placeholder="Search Quora" />
+      </div>
+      <div className="qHeader__Rem">
+        <div className="qHeader__avatar">
+          <Avatar
+            
+            className="Avatar"
+            src="https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573"
+         
+          />
+        </div>
+        <NotificationsOutlinedIcon />
+        <LanguageIcon />
+        <Button>Add Question</Button>
 
         <div>
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
@@ -56,9 +75,10 @@ function Navbar() {
             </li>
           </ul>
         </div>
+       
       </div>
-    </nav>
-  )
+    </div>
+  );
 }
 
 export default Navbar
--- social-media-frontend/src/index.js
+++ social-media-frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
-import "./App.css"
 
 import "bootstrap/dist/css/bootstrap.css"
+import App from "./App"
+import "./App.css"
 import "./stylesheet/form.css"
 
 import { Provider } from "react-redux"
